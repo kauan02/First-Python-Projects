77a121fa8b4a6a0eb2468a4f2e6c37b483d80c19
Kauan Barbosa Rezende <81306849+kauan02@users.noreply.github.com>
2023-05-29 10:51:15 -0300

    Update Convidados.py

--- Convidados.py
+++ Convidados.py
@@ -1,3 +1,6 @@
+# @Author: Kauan Rezende
+# @Date:   2023-05-29 10:50:55
+
 convidado = input("Escreva o nome do convidado: ")
 #caso tenha mais participantes na lista de convidados, adiciona-los abaixo entre as aspas
 if convidado in ["Miriam", "Raphael", "Marcos", "Kauan", "", "", "", "", "", "", "", "", "",]:
